7dde8d66f89bfe19f3807c1cb11fb6c1e1ee4dc7
Xyzuka <yeebrandon@outlook.com>
2022-09-19 19:51:35 -0700

    die rendering

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Die(prop) {
   return (
-    <div className='die'>{prop.value}</div>
+    <div className='die'>
+      <h2>{prop.value}</h2>
+    </div>
   )
 }
