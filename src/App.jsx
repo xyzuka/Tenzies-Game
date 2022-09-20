d47325da1f1ae2ea44dbacb3a06ac4f029cd36ee
Xyzuka <yeebrandon@outlook.com>
2022-09-20 13:25:58 -0700

    generating random die w ID

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,49 @@
 import './App.css'
 import Die from './components/Die'
-
-/**
- * Challenge:
- * 
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components 
- *   to look like they do in the slide
- *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *      - Use flexbox on main to center the dice container
- *        in the center of the page
- */
+import React from 'react'
+import { nanoid } from 'nanoid'
 
 function App() {
+  const [diceNumbers, setDiceNumbers] = React.useState(allNewDice)
+
+  function allNewDice() {
+    const newDiceObject = [];
+
+    for (let i = 0; i < 10; i++) {
+      const randomDice = Math.ceil(Math.random() * 6);
+
+      const newDieRoll = {
+        value: randomDice,
+        isHeld: false,
+        nanoid: nanoid()
+      }
+
+      newDiceObject.push(newDieRoll);
+    }
+
+    return newDiceObject
+  }
+
+  const renderedDice = diceNumbers.map((die) => {
+    return <Die
+      value={die.value}
+      isHeld={die.isHeld}
+      key={die.nanoid}
+    />
+  })
+
+  function resetDiceNumbers() {
+    setDiceNumbers(prevDiceNumbers => {
+      return prevDiceNumbers = allNewDice()
+    })
+  }
+ 
   return (
       <main className='game-container'>
         <div className="die-container">
-          <Die value={1}/>
-          <Die value={2}/>
-          <Die value={3}/>
-          <Die value={4}/>
-          <Die value={5}/>
-          <Die value={6}/>
-          <Die value={7}/>
-          <Die value={8}/>
-          <Die value={9}/>
-          <Die value={10}/>
+          {renderedDice}
         </div>
+        <button onClick={resetDiceNumbers}>Roll</button>
       </main>
   )
 }
