f720178d9ffc205d8a191aa3cec7a6908f602d70
Xyzuka <yeebrandon@outlook.com>
2022-09-21 00:17:46 -0700

    holding dice

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,66 @@ import Die from './components/Die'
 import React from 'react'
 import { nanoid } from 'nanoid'
 
+/**
+ * Challenge: Update the `rollDice` function to not just roll
+ * all new dice, but instead to look through the existing dice
+ * to NOT role any that are being `held`.
+ * 
+ * Hint: this will look relatively similiar to the `holdDice`
+ * function below. When creating new dice, remember to use
+ * `id: nanoid()` so any new dice have an `id` as well.
+ */
+
 function App() {
-  const [diceNumbers, setDiceNumbers] = React.useState(allNewDice)
+  const [diceNumbers, setDiceNumbers] = React.useState(allNewDice())
+
+  function generateNewDie() {
+    const randomDice = Math.ceil(Math.random() * 6);
+    return {
+        value: randomDice,
+        isHeld: false,
+        id: nanoid()
+    }
+  }
 
   function allNewDice() {
     const newDiceObject = [];
-
     for (let i = 0; i < 10; i++) {
-      const randomDice = Math.ceil(Math.random() * 6);
-
-      const newDieRoll = {
-        value: randomDice,
-        isHeld: false,
-        nanoid: nanoid()
-      }
-
-      newDiceObject.push(newDieRoll);
+      // const newDieRoll = generateNewDie();
+      newDiceObject.push(generateNewDie());
     }
-
     return newDiceObject
+  }
+
+  function resetDiceNumbers() {
+    setDiceNumbers(allNewDice())
+  }
+
+  
+  ////
+
+  // function resetDiceNumbers() {
+  //   setDiceNumbers(prevDiceNumbers => prevDiceNumbers.map(die => {
+  //     return die.isHeld ? die : allNewDice
+  //   }))
+  // }
+
+  function holdDice(id) {
+    setDiceNumbers(prevDiceNumbers => prevDiceNumbers.map(die => {
+      return die.id === id ?
+      {...die, isHeld: !die.isHeld} :
+      die
+    }))
   }
 
   const renderedDice = diceNumbers.map((die) => {
     return <Die
       value={die.value}
       isHeld={die.isHeld}
-      key={die.nanoid}
+      key={die.id}
+      holdDice={() => holdDice(die.id)}
     />
   })
-
-  function resetDiceNumbers() {
-    setDiceNumbers(prevDiceNumbers => {
-      return prevDiceNumbers = allNewDice()
-    })
-  }
  
   return (
       <main className='game-container'>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 export default function Die(prop) {
+  const styles = {
+    backgroundColor: prop.isHeld ? "#59E391" : "white"
+  }
+  
   return (
-    <div className='die'>
+    <div 
+      className="die" 
+      style={styles}
+      id={prop.id}
+      onClick={prop.holdDice}
+    >
       <h2>{prop.value}</h2>
     </div>
   )
